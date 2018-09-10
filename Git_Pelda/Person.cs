6b9cfa8f0e046edb00894dc32157122b7de7deda
Czeglédi Viktor <czegledi.viktor@gmail.com>
2018-09-10 20:06:46 +0200

    X

--- Git_Pelda/Person.cs
+++ Git_Pelda/Person.cs
@@ -11,5 +11,6 @@ namespace Git_Pelda
         public string Name { get; set; }
         public int Age { get; set; }
         public bool Male { get; set; }
+        public int X { get; set; }
     }
 }
