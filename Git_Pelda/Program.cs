9f0f25a1072f5ab990405b4c5f3f3bce0f8f8525
Czeglédi Viktor <czegledi.viktor@gmail.com>
2018-09-10 17:58:49 +0200

    Program hello átírva

--- Git_Pelda/Program.cs
+++ Git_Pelda/Program.cs
@@ -10,7 +10,7 @@ namespace Git_Pelda
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello Person");
         }
     }
 }
